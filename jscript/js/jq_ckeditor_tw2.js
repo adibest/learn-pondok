098597c3de994b17bc77649f2b7df410edafcc15
adibest <ahmadjayadibya@gmail.com>
2019-03-06 21:34:00 +0700

    alhamdulillah update finish jq

--- jscript/js/jq_ckeditor_tw2.js
+++ jscript/js/jq_ckeditor_tw2.js
@@ -9,11 +9,11 @@ $(document).ready(function() {
     $(".error").remove();
  
     if (nama_depan.length < 1) {
-      $('#nama_depan').after('<span class="error">Harap diisi!</span>');
+      $('#nama_depan').after('<span class="error"> Harap diisi!</span>');
     }
 
     if (email.length < 1) {
-      $('#email').after('<span class="error">Harap diisi</span>');
+      $('#email').after('<span class="error"> Harap diisi</span>');
     } else {
       var regEx = /^[A-Z0-9][A-Z0-9._%+-]{0,63}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/;
       var validEmail = regEx.test(email);
